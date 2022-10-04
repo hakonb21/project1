a484114c2d1df963e479c97f14afcdf69fb65c7c
Ymmiblubbs <ymir21@ru.is>
2022-10-04 12:57:37 +0000

    m

--- structured_logging/processors/enviroment_processor.py
+++ structured_logging/processors/enviroment_processor.py
@@ -2,6 +2,9 @@ from structured_logging.processors.abstract_processor import AbstractProcessor
 from structured_logging.configuration.environment import Environment
 
 class EnviromentProcessor(AbstractProcessor):
+    def __init__(self, enviroment):
+        self.enviroment = enviroment
+
     def handle(self, data: dict):
-        data["enviroment"] = Environment.staging.value
+        data["enviroment"] = self.enviroment
         return super().handle(data)
